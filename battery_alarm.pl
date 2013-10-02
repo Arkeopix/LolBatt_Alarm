6a81d2ab705a305125962b6215ede8e571d68352
Arkeopix <mendiej750@gmail.com>
2013-10-02 16:38:44 +0200

    Add fork

--- battery_alarm.pl
+++ battery_alarm.pl
@@ -7,22 +7,43 @@ use Audio::Beep;
 
 while (42) {
     my @paths = ("/sys/class/power_supply/BAT0/charge_now", 
-		 "/sys/class/power_supply/BAT0/charge_full");
+		 "/sys/class/power_supply/BAT0/charge_full",
+		 "/sys/class/power_supply/BAT0/status");
     my @values;
     
     for (my $x = 1; $x < 3; $x++) {
-	open(BATT, $paths[$x -1]) or die "Battery File not found\n";
+	open(BATT, $paths[$x -1]) 
+	    or die "Battery File ($paths[$x -1])not found\n";
 	while (<BATT>) {
 	    $values[$x -1] = $_;
 	}
 	close(BATT);
     }
     
-    if (($values[0] / $values[1]) * 100 <= 10) {
-	my $beeper = Audio::Beep->new();
-	my $music = q|
-	g g g d|; 
-	$beeper->play($music);
+    if (($values[0] / $values[1]) * 100 <= 100) {
+	my $pid = fork();
+	if ($pid == 0) {
+	    my $beeper = Audio::Beep->new();
+	    my $music = q|
+	      g g g d|; 
+	    $beeper->play($music);
+	} else {
+	    my $done = 0;
+	    my $str;
+	    while ($done == 0) {
+		open(STATUS, $paths[2]) or die "Could not open $paths[2]\n"; 
+		while (<STATUS>) {
+		    $str = $_;
+		}
+		close(STATUS);
+		if ($str eq "Charging\n") {
+		    print "coucou\n";
+		    kill 9, $pid;
+		    $done = 1;
+		}
+	    }
+	    wait();
+	}
     }
     sleep(60);
 }
